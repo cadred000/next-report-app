a57b958195ca3ceb22a21bf0cb85c3ca971255bc
cadred000 <cadred000@protonmail.com>
2025-01-24 01:16:24 -0700

    hate this

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+	output: 'export',
+	basePath: '/next-report-app',
+	assetPrefix: '/next-report-app',
 };
 
 export default nextConfig;
